1382b73b76544c5e71fc4d7da53611c42ef66eaf
Siri-chan <siri1999kira@yahoo.co.jp>
2026-01-28 19:17:48 +1100

    Generate quickstart maven archetype.

---
+++ src/main/java/io/github/baconpeeks/ci_guard/App.java
@@ -0,0 +1,10 @@
+package io.github.baconpeeks.ci_guard;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
